66884f19cfcb21a2338efea5fdade9fe80d0e3e8
BloxdioCannoli <229039173+BloxdioCannoli@users.noreply.github.com>
2026-08-06 18:08:43 -0400

    Overwrite remote repository with local files

--- script.js
+++ script.js
@@ -1,6 +1,106 @@
+// Query detection setup with the URL
+const url = window.location.href;
+let queryString = url.split("?")[1]?.split("#")[0];
+let queryObject = {};
+
+if (queryString) {
+    queryObject = queryStringToQueryObject(queryString)
+}
+
+function queryStringToQueryObject(queryString) {
+    let queryObject = {};
+    queryString = queryString.split(",")
+
+    for (let part of queryString) {
+        if (part.includes("=")) {
+            part = part.split("=");
+            queryObject[part[0]] = part[1];
+        } else {
+            queryObject[part] = true;
+        }
+    }
+
+    return queryObject;
+}
+
+function queryObjectToQueryString(queryObject) {
+    let params = "";
+    for (let key in queryObject) {
+        if (queryObject[key] === true) {
+            params += `${key},`
+        } else {
+            params += `${key}=${queryObject[key]},`
+        }
+    }
+
+    return `${params.slice(0, params.length - 1)}`;
+}
+
+// Sections
+globalThis.sections = {}
+for (let section of document.getElementsByClassName("section")) {
+    let data = section.dataset
+    let sectionName = data?.section
+
+    let selectedSection = queryObject.section
+
+    if (!sectionName) { continue }
+    let isSelected = sectionName === selectedSection
+    sections[sectionName] = {
+        elem: section,
+        isSelected,
+    }
+
+    if (isSelected) {
+        section.animate([
+            {
+                offset: 0,
+
+                opacity: 1,
+
+            },
+            {
+                offset: 0.7,
+
+                opacity: 0.6,
+            },
+            {
+                offset: 1,
+
+                opacity: 1,
+            }
+        ], {
+            duration: 1500,
+            iterations: 3,
+        })
+    }
+}
+
+// Social button activation
 for (let socialButton of document.getElementsByClassName("socialButton")) {
+    let data = socialButton.dataset
     socialButton.addEventListener("click", () => {
-        let data = socialButton.dataset
         window.open(data?.href ?? "#", "_blank")
+    })
+}
+
+for (let faqGroup of document.getElementsByClassName("faq")) {
+    let header = faqGroup.getElementsByClassName("faq-header")[0]
+    let content = faqGroup.getElementsByClassName("faq-content")[0]
+
+    let innerHTML = header.innerHTML
+    header.innerHTML = `<img src="img/white-arrow.png" class="faq-arrow"> ${innerHTML}`
+
+    content.style.display = "none"
+    header.className = "faq-header"
+    header.addEventListener("click", () => {
+        let display = content.style.display
+        if (display === "none") {
+            content.style.display = "block"
+            header.className = "faq-header selected"
+        } else {
+            content.style.display = "none"
+            header.className = "faq-header"
+        }
     })
 }
